058c9718af72db02f37b5f76948372ba9e919cb5
soloxdipayan <ark.mandal.3@gmail.com>
2024-07-31 01:40:20 +0530

    exercise 5

---
+++ week1_assesment/DecoratorPatternExample/SMSNotifierDecorator.java
@@ -0,0 +1,9 @@
+public class SMSNotifierDecorator extends NotifierDecorator {
+    public SMSNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+    public void send(String message) {
+        super.send(message);
+        System.out.println("Sending SMS: " + message);
+    }
+}
---
+++ week1_assesment/DecoratorPatternExample/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        Notifier emailNotifier = new EmailNotifier();
+        Notifier smsNotifier = new SMSNotifierDecorator(emailNotifier);
+        Notifier slackNotifier = new SlackNotifierDecorator(smsNotifier);
+
+        slackNotifier.send("Hello, this is a test notification!");
+    }
+
+}
