8bbdcaac4b1b5cbcf1621cf687ecd2f84f94cef8
soloxdipayan <ark.mandal.3@gmail.com>
2024-07-30 20:56:38 +0530

    Exercise 1

---
+++ week1_assesment/SingletonPatternExample/Logger.java
@@ -0,0 +1,25 @@
+public class Logger {
+    private static Logger instance; // instance - logger class
+    
+    private Logger(){
+        // constructor
+    }
+    // method to get the instance
+     public static Logger getInstance() 
+     {
+        if(instance == null){
+            instance = new Logger();
+        }
+        return instance;
+     }
+    //  methode to verify that only one instance of Logger 
+    // is created and used across the application.
+    public static void logMsg(String msg){
+        System.out.println("log- "+ msg);
+    }
+}
+
+
+// test class to verify that only one instance of Logger is 
+// created and used across the application
+
---
+++ week1_assesment/SingletonPatternExample/LoggerTest.java
@@ -0,0 +1,13 @@
+public class LoggerTest {
+    public static void main(String args[]){
+        Logger log1=Logger.getInstance();
+        log1.logMsg("1 log");
+        Logger log2 = Logger.getInstance();
+        log2.logMsg("2 log");
+        if(log1==log2)
+        System.out.println("both logger is the same instance.");
+        else
+        System.out.println("both are different instance.");
+
+    }
+}
