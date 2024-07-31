dfdc84b5481978466ffc4c0189f0db0febdbd0ca
soloxdipayan <ark.mandal.3@gmail.com>
2024-07-31 13:38:32 +0530

    exercise 7

---
+++ week1_assesment/Design_Patterns_and_Principles/ObserverPatternExample/test.java
@@ -0,0 +1,18 @@
+public class test {
+    public static void main(String[] args) {
+        StockMarket stockMarket = new StockMarket();
+
+        Observer mobileApp = new MobileApp("MobileApp");
+        Observer webApp = new WebApp("WebApp");
+
+        stockMarket.register(mobileApp);
+        stockMarket.register(webApp);
+
+        stockMarket.setStockPrice(100.50);
+        stockMarket.setStockPrice(102.75);
+
+        stockMarket.deregister(mobileApp);
+        stockMarket.setStockPrice(105.00);
+    }
+
+}
