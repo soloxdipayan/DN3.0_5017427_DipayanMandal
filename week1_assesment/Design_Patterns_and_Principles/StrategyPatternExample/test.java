b9639ced79f561a7d0f467b3d0bbc3bd2104e380
soloxdipayan <ark.mandal.3@gmail.com>
2024-07-31 13:49:44 +0530

    exercise 8

---
+++ week1_assesment/Design_Patterns_and_Principles/StrategyPatternExample/test.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String[] args) {
+        PaymentContext context = new PaymentContext();
+
+        // Pay via Credit Card
+        context.setPaymentStrategy(new CreditCardPayment("1234-5678-9012-3456", "John Doe"));
+        context.executePayment(103.0);
+
+        // Pay via PayPal
+        context.setPaymentStrategy(new PayPalPayment("john.doe@example.com"));
+        context.executePayment(302.0);
+    }
+}
