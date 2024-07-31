ed18b0b7fe429bb3f9d8467a4abc36ae409fab1d
soloxdipayan <ark.mandal.3@gmail.com>
2024-07-31 13:09:04 +0530

    test update

--- week1_assesment/Design_Patterns_and_Principles/ProxyPatternExample/test.java
+++ week1_assesment/Design_Patterns_and_Principles/ProxyPatternExample/test.java
@@ -1,17 +1,17 @@
 public class test {
     public static void main(String[] args) {
         Image image1 = new ProxyImage("image1.jpg");
-        Image image2 = new ProxyImage("image2.jpg");
+        // Image image2 = new ProxyImage("image2.jpg");
 
-        
+    // to load img from remote servers
+    
         image1.display();
         System.out.println("");
 
-        
-        image2.display();
-        System.out.println("");
+        // image2.display();
+        // System.out.println("");
 
-        
+        // load from cache
         image1.display();
     }
 
